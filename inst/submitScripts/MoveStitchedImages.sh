4e0877863545e5b6310512421bebc8e98200bbd5
GWMcElfresh <mcelfreshgw@gmail.com>
2025-03-18 11:51:23 -0700

    add TODO about changing MoveStitchedImages into a batch script

--- inst/submitScripts/MoveStitchedImages.sh
+++ inst/submitScripts/MoveStitchedImages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#TODO: this script needs to be abstracted to submit a job per directory, and then move the stitched images to the prime-seq workbook.
 # Usage: ./move_stitched_images_and_rds.sh <source_directory> <destination_directory>
 
 SOURCE_DIR="$1"
@@ -19,7 +19,7 @@ find "$SOURCE_DIR" -type d -name "StitchedImage" | while read -r stitched_dir; d
   #create the corresponding directory structure in the prime-seq workbook
   mkdir -p "$DEST_DIR/$relative_path"
 
-  # Copy the stitchedImage folder to prime-seq, preserving hierarchy
+  #copy the stitchedImage folder to prime-seq, preserving hierarchy
   cp -r "$stitched_dir" "$DEST_DIR/$relative_path/"
 
   #find and copy any .RDS files in the same parent directory
